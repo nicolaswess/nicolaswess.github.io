4cc97c3817617fddb4dc56c26fae14726a264b67
April Larsen <hunterapril94@gmail.com>
2022-11-08 14:46:42 -0800

    fix menu and add transitions and stuff

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,21 +1,17 @@
 import { useState } from "react"
 
 export default function Nav() {
-  const [isActive, setActive] = useState(true)
   function handleClick() {
-    setActive(!isActive)
-    console.log(isActive)
+    document.getElementById("myDropdown").classList.toggle("show");
   }
   return(
-    <div className="nav" width='75px'>
-      <button onClick={handleClick}>
-      <img src="../menuIconWhite.png" />
-      </button>
-      <div className={isActive ? "hidden menu-items" : "menu-items"}>
-        <a href='#about'><h2>About Me</h2></a>
-        <a href='#research'><h2>Research</h2></a>
-        <a href='#cv'><h2>CV</h2></a>
-      </div>
-    </div>
+<div class="nav">
+  <button onClick={handleClick} class="dropbtn"><img src='../menuIconWhite.png'></img></button>
+  <div id="myDropdown" class="dropdown-content">
+    <a href="#about">About</a>
+    <a href="#research">Research</a>
+    <a href="#cv">CV</a>
+  </div>
+</div>
   )
 }
