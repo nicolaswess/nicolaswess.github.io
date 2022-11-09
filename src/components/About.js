1147dfdbaebde518cc312c8b868a4e423a0f2e50
April Larsen <hunterapril94@gmail.com>
2022-11-08 16:24:25 -0800

    ok for real though

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ export default function About() {
       <img src='../Nicologo.png' alt='Headshot' height='300px' />
       <h3>UBC Sauder School of Business</h3>
       <hr style={{border: '1px solid white', width: '75%'}} />
-      <h4>Strategy and Business Economics PhD Student</h4>
+      <h4>PhD student in Strategy and Business Economics</h4>
       <p>
         My research interests lie at the intersection of International Trade and Political Economy, primarily concerning the effects of trade on political outcomes.
         I am also interested in the economics of crime, in particular with respect to international drug trafficking. 
